7f337b72b2e5c28f8e4272a291686d34654de756
SaraAldaz <sarvaldazc124@umariana.edu.co>
2026-05-15 22:27:23 -0500

    cambio base de datos y crud para todo

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,102 @@ app.get("/", (req, res) => {
     res.send("Hola mundo");
 });
 
+app.get("/categorias", (req, res) => {
+
+    const sql = `
+        SELECT * FROM categorias
+    `;
+
+    db.query(sql, (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+            res.json(resultado);
+        }
+
+    });
+
+});
+
+app.post("/categorias", (req, res) => {
+
+    const { nombreCategoria } = req.body;
+
+    const sql = `
+        INSERT INTO categorias(nombreCategoria)
+        VALUES(?)
+    `;
+
+    db.query(sql, [nombreCategoria], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Categoría agregada"
+            });
+
+        }
+
+    });
+
+});
+
+app.delete("/categorias/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    const sql = `
+        DELETE FROM categorias
+        WHERE idCategoria = ?
+    `;
+
+    db.query(sql, [id], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Categoría eliminada"
+            });
+
+        }
+
+    });
+
+});
+
+app.put("/categorias/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    const { nombreCategoria } = req.body;
+
+    const sql = `
+        UPDATE categorias
+        SET nombreCategoria = ?
+        WHERE idCategoria = ?
+    `;
+
+    db.query(sql, [nombreCategoria, id], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Categoría actualizada"
+            });
+
+        }
+
+    });
+
+});
+
 app.get("/productos", (req, res) => {
     const sql = `
         select productos.*, categorias.nombreCategoria
@@ -28,11 +124,11 @@ app.get("/productos", (req, res) => {
 
 
 app.post("/productos", (req, res) => {
-    const {nombre, precio, stock, foto, idCategoria} = req.body;
+    const {nombreProducto, precio, stock, foto, idCategoria} = req.body;
     const sql = `
-        insert into productos(nombre, precio, stock, foto, idCategoria) values (?,?,?,?,?) `;
+        insert into productos(nombreProducto, precio, stock, foto, idCategoria) values (?,?,?,?,?) `;
     
-    db.query(sql, [nombre, precio, stock, foto, idCategoria],
+    db.query(sql, [nombreProducto, precio, stock, foto, idCategoria],
     (err, respuesta) => {
         if(err){
             console.error("Problemitas", err);
@@ -60,10 +156,10 @@ app.delete("/productos/:idProducto", (req, res) => {
 
 app.put("/productos/:idProducto", (req, res) => {
     const id = req.params.idProducto;
-    const {nombre, precio, stock, foto, idCategoria} = req.body;
-    const sql = `update productos set nombre = ?, precio = ?, stock = ?,
+    const {nombreProducto, precio, stock, foto, idCategoria} = req.body;
+    const sql = `update productos set nombreProducto = ?, precio = ?, stock = ?,
      foto = ?, idCategoria = ? where idProducto = ?`;
-    db.query(sql, [nombre, precio, stock, foto, idCategoria, id], 
+    db.query(sql, [nombreProducto, precio, stock, foto, idCategoria, id], 
         (err, respuesta) => {
         if(err){
             console.error("Problemitas", err);
@@ -73,6 +169,198 @@ app.put("/productos/:idProducto", (req, res) => {
             });
         }
     });
+});
+
+app.get("/vendedores", (req, res) => {
+
+    const sql = `
+        SELECT * FROM vendedores
+    `;
+
+    db.query(sql, (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+            res.json(resultado);
+        }
+
+    });
+
+});
+
+app.post("/vendedores", (req, res) => {
+
+    const { nombre, apellido, correo, telefono } = req.body;
+
+    const sql = `
+        INSERT INTO vendedores(nombre, apellido, correo, telefono)
+        VALUES(?, ?, ?, ?)
+    `;
+
+    db.query(sql, [nombre, apellido, correo, telefono], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Vendedor agregado"
+            });
+
+        }
+
+    });
+
+});
+
+app.delete("/vendedores/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    const sql = `
+        DELETE FROM vendedores  
+        WHERE idVendedor = ?
+    `;
+
+    db.query(sql, [id], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Vendedor eliminado"
+            });
+
+        }
+
+    });
+
+});
+
+app.put("/vendedores/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    const { nombre, apellido, correo, telefono } = req.body;
+
+    const sql = `
+        UPDATE vendedores
+        SET nombre = ?, apellido = ?, correo = ?, telefono = ?
+        WHERE idVendedor = ?
+    `;
+
+    db.query(sql, [nombre, apellido, correo, telefono, id], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Vendedor actualizado"
+            });
+
+        }
+
+    });
+
+});
+
+app.get("/ventas", (req, res) => {
+
+    const sql = `
+        SELECT * FROM ventas
+    `;
+
+    db.query(sql, (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+            res.json(resultado);
+        }
+
+    });
+
+});
+
+app.post("/ventas", (req, res) => {
+
+    const { fechaVenta, total, idProducto, idVendedor } = req.body;
+
+    const sql = `
+        INSERT INTO ventas(fechaVenta, total, idProducto, idVendedor)
+        VALUES(?, ?, ?, ?)
+    `;
+
+    db.query(sql, [fechaVenta, total, idProducto, idVendedor], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Venta agregada"
+            });
+
+        }
+
+    });
+
+});
+
+app.delete("/ventas/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    const sql = `
+        DELETE FROM ventas
+        WHERE idVenta = ?
+    `;
+
+    db.query(sql, [id], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Venta eliminada"
+            });
+
+        }
+
+    });
+
+});
+
+app.put("/ventas/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    const { fechaVenta, total, idProducto, idVendedor } = req.body;
+
+    const sql = `
+        UPDATE ventas
+        SET fechaVenta = ?, total = ?, idProducto = ?, idVendedor = ?
+        WHERE idVenta = ?
+    `;
+
+    db.query(sql, [fechaVenta, total, idProducto, idVendedor, id], (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        } else {
+
+            res.json({
+                mensaje: "Venta actualizada"
+            });
+
+        }
+
+    });
+
 });
 
 app.listen(3000, () => {
